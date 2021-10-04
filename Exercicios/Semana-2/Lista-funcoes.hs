1075346213c8aa9ea5a40ca7b7da2b187a067d29
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2021-10-04 14:43:08 -0300

    Adiciona questões 10, 11, 12e 13

--- Exercicios/Semana-2/Lista-funcoes.hs
+++ Exercicios/Semana-2/Lista-funcoes.hs
@@ -75,6 +75,45 @@ potencia :: Integer -> Integer -> Integer
 potencia n k = tailPotencia n k 1
 {-------------}
 
+{- Questão 10 -}
+coeficienteBinomial :: Integer -> Integer -> Integer
+coeficienteBinomial n 0 = 1
+coeficienteBinomial 0 k = 0
+coeficienteBinomial n k = (coeficienteBinomial (n-1) k) + (coeficienteBinomial (n-1) (k-1))
+{-------------}
+
+{- Questão 11 -}
+tailTribonacci :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
+tailTribonacci parada anterior2 anterior1 acumulador indece
+ | indece == parada = acumulador
+ | otherwise = tailTribonacci
+                             (parada) 
+                             (anterior1)  
+                             (acumulador) 
+                             (anterior2 + anterior1 + acumulador)
+                             (indece + 1)
+
+tribonacci :: Integer -> Integer
+tribonacci 1 = 1
+tribonacci 2 = 1
+tribonacci 3 = 2
+tribonacci x = tailTribonacci (x) (1) (2) (4) (4)
+{-------------}
+
+{- Questão 12 -}
+addEspacosAux :: Int -> String -> String
+addEspacosAux 0 s = s
+addEspacosAux x s = addEspacosAux (x-1) (s ++ " ")
+
+addEspacos :: Int -> String
+addEspacos x = addEspacosAux x ""
+{-------------}
+
+{- Questão 13 -}
+paraDireita :: Int -> String -> String
+paraDireita x s = addEspacos x ++ s
+{-------------}
+
 {- Questão x -}
 
 {-------------}
